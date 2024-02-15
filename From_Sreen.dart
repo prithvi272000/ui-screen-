653f5e836a4db8dc909be74ac61f0e4848add552
prithvi272000 <160127741+prithvi272000@users.noreply.github.com>
2024-02-15 17:14:52 +0530

    Add files via upload

---
+++ From_Sreen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class FromScreen extends StatefulWidget {
+  @override
+  State<FromScreen> createState() => _FromScreenState();
+}
+
+class _FromScreenState extends State<FromScreen> {
+  final _formfield = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form Page"),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+            child: Form(
+              key: _formfield,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/profile.jpg", height: 100, width: 100,),
+                  SizedBox(height: 50),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Enter your email id";
+                      }
+                      bool emailValid = RegExp(
+                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"
+                      ).hasMatch(value);
+                      if (!emailValid) {
+                        return "Enter a valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: passController,
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: Icon(passToggle ? Icons.visibility : Icons.visibility_off),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Enter your password";
+                      }
+                      if (value.length < 6) {
+                        return "Your password is weak";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 60),
+                  InkWell(
+                    onTap: () {
+                      if (_formfield.currentState?.validate() ?? false) {
+                        // If the form is valid, perform the login action
+                        // You can add your login logic here
+                        print("Login successful!");
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
